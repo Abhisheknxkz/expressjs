5d02911412f612651f9d0590c7de83c7c1cde9fa
DaniellaZambrano <idaniellazambrano@gmail.com>
2025-01-03 01:35:03 -0400

    DEV-2: + added POST requests

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require(`express`)
 
 const app = express()
 app.use(express.json())
+app.use(express.text())
 
 // String array
 const stringArray = ["Saab", "Volvo", "BMW"];
@@ -25,6 +26,7 @@ const objectArray = [
 // Number array
 const numberArray = [40, 100, 1, 5, 25, 10];
 
+// GET requests
 app.get('/api/strings', (req, res) => {
 
     res.json(stringArray);
@@ -42,6 +44,58 @@ app.get('/api/objects', (req, res) => {
 app.get('/api/numbers', (req, res) => {
 
     res.json(numberArray);
+    
+    res.end();
+});
+
+// POST requests
+app.post('/api/json', (req, res) => {
+
+    const body = req.body;
+
+    if (typeof body !== 'object' || !body)
+    {
+        res.status(400).write("body must be JSON format");
+    }
+    else
+    {
+        console.log(body);
+    }
+
+
+    res.end();
+});
+
+app.post('/api/string', (req, res) => {
+
+    const body = req.body;
+
+    if (typeof body != 'string' || !body)
+    {
+        res.status(400).write("body must be string format");
+    }
+    else
+    {
+        console.log(body);
+    }
+    
+    res.end();
+});
+
+app.post('/api/header', (req, res) => {
+
+    const header = req.headers;
+    const body = req.body;
+
+    if (header.somekey !== "secret")
+    {
+        res.status(404).write("wrong key");
+    }
+    else
+    {
+        console.log(body);    
+        console.log(header);
+    }
     
     res.end();
 });
