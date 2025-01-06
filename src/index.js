ce560edb225a2c1d895b2257eb30e8f6a1714973
DaniellaZambrano <idaniellazambrano@gmail.com>
2025-01-06 05:31:20 -0400

    DEV-5: + CRUD example

--- src/index.js
+++ src/index.js
@@ -4,157 +4,132 @@ const app = express()
 app.use(express.json())
 app.use(express.text())
 
-// String array
-const stringArray = ["Saab", "Volvo", "BMW"];
-
-// Object array
-const objectArray = [
+// Static array
+const arrayData = [
     {
-      color: "purple",
-      type: "minivan",
-      registration: new Date('2017-01-03'),
-      capacity: 7
+      postId: 1,
+      id: 1,
+      name: "id labore ex et quam laborum",
+      email: "Eliseo@gardner.biz",
+      body: "laudantium enim quasi est quidem magnam voluptate ipsam eos\ntempora quo necessitatibus\ndolor quam autem quasi\nreiciendis et nam sapiente accusantium",
+      hide: false
     },
     {
-      color: "red",
-      type: "station wagon",
-      registration: new Date('2018-03-03'),
-      capacity: 5
+      postId: 1,
+      id: 2,
+      name: "quo vero reiciendis velit similique earum",
+      email: "Jayne_Kuhic@sydney.com",
+      body: "est natus enim nihil est dolore omnis voluptatem numquam\net omnis occaecati quod ullam at\nvoluptatem error expedita pariatur\nnihil sint nostrum voluptatem reiciendis et",
+      hide: false
     },
+    {
+      postId: 1,
+      id: 3,
+      name: "odio adipisci rerum aut animi",
+      email: "Nikita@garfield.biz",
+      body: "quia molestiae reprehenderit quasi aspernatur\naut expedita occaecati aliquam eveniet laudantium\nomnis quibusdam delectus saepe quia accusamus maiores nam est\ncum et ducimus et vero voluptates excepturi deleniti ratione",
+      hide: false
+    }
   ];
 
-// Number array
-const numberArray = [40, 100, 1, 5, 25, 10];
-
-// GET requests
-app.get('/api/strings', (req, res) => {
-
-    res.json(stringArray);
+// CRUD
+app.get('/posts', (req, res) => {
+    const visiblePosts = arrayData.filter(item => !item.hide);
+    res.json(visiblePosts);
 
     res.end();
 });
 
-app.get('/api/objects', (req, res) => {
-
-    res.json(objectArray);
+app.get('/posts/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const post = arrayData.find(item => item.id === id && !item.hide);
     
-    res.end();
-});
-
-app.get('/api/numbers', (req, res) => {
-
-    res.json(numberArray);
+    if (!post)
+        return res.status(404).json({ error: 'Error, post not found' });
     
-    res.end();
-});
-
-// POST requests
-app.post('/api/json', (req, res) => {
-
-    const body = req.body;
-
-    if (typeof body !== 'object' || !body)
-    {
-        res.status(400).write("body must be JSON format");
-    }
-    else
-    {
-        console.log(body);
-    }
-
+    res.json(post);
 
     res.end();
 });
 
-app.post('/api/string', (req, res) => {
-
-    const body = req.body;
-
-    if (typeof body != 'string' || !body)
-    {
-        res.status(400).write("body must be string format");
-    }
-    else
-    {
-        console.log(body);
-    }
-    
-    res.end();
-});
-
-app.post('/api/header', (req, res) => {
-
+app.post('/posts', (req, res) => {
     const header = req.headers;
-    const body = req.body;
-
     if (header.somekey !== "secret")
-    {
-        res.status(404).write("wrong key");
-    }
-    else
-    {
-        console.log(body);    
-        console.log(header);
-    }
+        return res.status(404).json({ error: 'Error, wrong key' });    
+
+    const { 
+        postId, 
+        name, 
+        email, 
+        body 
+    } = req.body;
+
+    if (!postId || !name || !email || !body) 
+        return res.status(400).json({ error: 'Error, require a field' });
     
-    res.end();
-});
-
-app.put('/api/:id/:firstname', (req, res) => {
-
-    const body = req.body;
-    const id = req.params.id;
-    const name = req.params.firstname;
-
-    res.json({
-        id,
+    const newPost = {
+        postId,
+        id: arrayData.length + 1,
         name,
-        body
-    });
+        email,
+        body,
+        hide: false
+    };
+
+    arrayData.push(newPost);
+    res.status(201).json({
+        message: 'Create successfully',
+        newPost,
+        arrayData
+    })
 
     res.end();
 });
 
-app.delete('/api/delete/:id', (req, res) => {
+app.put('/posts/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const postIndex = arrayData.findIndex(item => item.id === id);
 
-    const id = req.params.id;
+    if (postIndex === -1) 
+        return res.status(404).json({ error: 'Error, post not found' });
     
-    const data = [
-        {
-            "postId": 1,
-            "id": 1,
-            "name": "id labore ex et quam laborum",
-            "email": "Eliseo@gardner.biz",
-            "body": "laudantium enim quasi est quidem magnam voluptate ipsam eos\ntempora quo necessitatibus\ndolor quam autem quasi\nreiciendis et nam sapiente accusantium",
-            "hide": false
-          },
-          {
-            "postId": 1,
-            "id": 2,
-            "name": "quo vero reiciendis velit similique earum",
-            "email": "Jayne_Kuhic@sydney.com",
-            "body": "est natus enim nihil est dolore omnis voluptatem numquam\net omnis occaecati quod ullam at\nvoluptatem error expedita pariatur\nnihil sint nostrum voluptatem reiciendis et",
-            "hide": false
-          },
-          {
-            "postId": 1,
-            "id": 3,
-            "name": "odio adipisci rerum aut animi",
-            "email": "Nikita@garfield.biz",
-            "body": "quia molestiae reprehenderit quasi aspernatur\naut expedita occaecati aliquam eveniet laudantium\nomnis quibusdam delectus saepe quia accusamus maiores nam est\ncum et ducimus et vero voluptates excepturi deleniti ratione",
-            "hide": false
-          },
-        ];
+    const { 
+        postId, 
+        name, 
+        email, 
+        body 
+    } = req.body;
 
-    const valueDelete = data.filter( (ids) => {
-        if (ids.id === parseInt(id))
-            ids.hide = true;
-        return ids;
-    });
+    if (!postId || !name || !email || !body) 
+        return res.status(400).json({ error: 'Error, require a field' });
+    
+    arrayData[postIndex] = {
+    ...arrayData[postIndex],
+    postId,
+    name,
+    email,
+    body
+    };
 
-    res.json(valueDelete);
+    res.json(arrayData[postIndex]);
 
-    res.end();    
+    res.end();
+});
+
+app.delete('/posts/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const postIndex = arrayData.findIndex(item => item.id === id);
+  
+    if (postIndex === -1)
+        return res.status(404).json({ error: 'Error, post not found' });
+    
+    if (arrayData[postIndex].hide == true)
+        return res.status(404).json({ error: 'Error, post not found (hide)' });
+    
+    arrayData[postIndex].hide = true; 
+    res.json({ message: 'Deleted post succesfully', arrayData }); 
+    
+    res.end();
 });
 
 app.listen(3000, () => {console.log("APi is working on port 3000")})
