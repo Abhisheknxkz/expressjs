a94c2cba66723f614c887b5edb43d1b89c8fb0d5
DevPeak Innovations <devpeakinnovations@gmail.com>
2025-01-08 11:01:35 +0000

    jwt token access

---
+++ src/products/auth/index.js
@@ -0,0 +1,39 @@
+const jwt = require('jsonwebtoken');
+const express = require('express');
+const { validateToken } = require('./helper');
+const app = express();
+
+const message = {
+    text: 'Hi! How are you ?',
+    token: 'sxncdjakfbw3hr8923ewjdfnosjkflb3uweklaDH32l'
+},protected = {
+    text: "Your route to /protected api"
+},user = {
+    id: 'daniella123',
+    password: 'psswrd098'
+}
+
+app.get('/api/security/home', (req, res) => {
+    res.json(message);
+    res.end('Route /api/security/home ends here');
+});
+
+app.post('/api/security/tokens', (req, res) => {
+    const token = jwt.sign({user}, 'my_secret_key1234', {expiresIn: "1h"});
+    res.status(201).json({token}).end();
+});
+
+//(route, middleware, callback)
+app.get('/daniella-account-access', validateToken, (req, res) => {
+    jwt.verify(req.token, 'my_secret_key1234', (err, data) => {
+        if(err) {
+            res.sendStatus(403);
+        } else {
+            res.json({protected, data})
+        }
+    })
+})
+
+app.listen(4000, ()=> {
+    console.log('listening on port 4000');
+})
